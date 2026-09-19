2776b5a5da8e4b8e87c5ea19512e818568903ba3
Manoj <kshmanojsingha@gmail.com>
2026-09-19 23:57:14 +0530

    day-04

---
+++ traffic.js
@@ -0,0 +1,30 @@
+var colour="red";
+if (colour==="red"){
+    console.log("STOP");
+}
+else if(colour==="yellow"){
+    console.log("GET READY");
+}
+else if(colour==="green"){
+    console.log("GO");
+}
+else{
+    console.log("Invalid Colour");
+}
+
+
+
+
+var age="60";
+if(age<5){
+    console.log("Ticket is free");
+}
+else if(age<=17){
+    console.log("Child Ticket");
+}
+else if(age<=59){
+    console.log("Adult Ticket");
+}
+else{
+    console.log("Senior Ticket");
+}
