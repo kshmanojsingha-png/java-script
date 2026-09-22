d0f0753eb0555c97f30a1120b0a25af401bedf4c
Manoj <kshmanojsingha@gmail.com>
2026-09-22 16:47:48 +0530

    Class work question no. 1

---
+++ ques1.js
@@ -0,0 +1,18 @@
+var student_name="Ramesh";
+let age=17;
+let course="BCA";
+const college_name="SITM";
+let marks=78;
+const birth_year= 2007;
+let city="Mumbai";
+var semester= 1;
+var favourite_subject="Java Script"
+age=19;
+marks=90;
+birth_year=2006;
+console.log(student_name);
+console.log(age);
+console.log(course);
+console.log(college_name);
+console.log(marks);
+console.log(birth_year);
